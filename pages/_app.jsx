24024e02a0a763e5e5fbc0af13b52979cf3f893c
vedi <shubin.fedor@gmail.com>
2020-03-17 09:37:49 +0300

    Integrate styled components

---
+++ pages/_app.jsx
@@ -0,0 +1,36 @@
+import App from 'next/app';
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+};
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
+  }
+}
